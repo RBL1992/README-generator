20fb14971cf02891ce603028bb7af700d53f9249
Roy <loganroyjr4@gmail.com>
2022-09-20 16:40:15 -0500

    added array of objects containing questions for the user

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
 const inquirer = require('inquirer')
 const fileSystem = require('fs')
 
+const questions = [
+    {
+        type: 'input',
+        message: 'Tilte of Project',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'Project description',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please provide installation instructions',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide usage info if applicable',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please provide contributor information',
+        name: 'contributors',
+    },
+    {
+        type: 'input',
+        message: 'Project description',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please provide testing info',
+        name: 'testing',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your project',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'NONE'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub username',
+        name: 'GitHubUsername',
+    },
+    {
+        type: 'input',
+        message: 'Plase enter your email',
+        name: 'email',
+    },
+];
+
